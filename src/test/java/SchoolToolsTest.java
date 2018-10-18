eb91339f9379b3d7fc89bef36d2c7e5740f7eee2
Daniel Katzberg <daniel.katzberg@web.de>
2018-10-18 10:56:36 +0200

    added junit5 to gradle and build one junit test for setup junit

---
+++ src/test/java/SchoolToolsTest.java
@@ -0,0 +1,13 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class SchoolToolsTest {
+	
+	//TODO Tutorial Test, only for set up junit 5 with gradle
+	@Test
+	void test() {
+		assertEquals(2, 1 + 1);
+	}
+
+}
